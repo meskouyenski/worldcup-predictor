5ee5f923189c63a48e665d2f6778b06144ba5324
vanstricker <meskouyenski@gmail.com>
2026-06-13 03:46:36 +0200

    fix api route

--- app/api/cron/fetch-scores/route.ts
+++ app/api/cron/fetch-scores/route.ts
@@ -1,18 +1,5 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  try {
-    return NextResponse.json({
-      success: true,
-      message: "cron working",
-    });
-  } catch (error: any) {
-    return NextResponse.json(
-      {
-        success: false,
-        error: error.message,
-      },
-      { status: 500 }
-    );
-  }
+  return NextResponse.json({ ok: true });
 }
