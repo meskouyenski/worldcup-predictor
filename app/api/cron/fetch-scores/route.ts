89ccb8d62f1482c3bcf1b797daf4c489db12c619
vanstricker <meskouyenski@gmail.com>
2026-06-13 03:41:23 +0200

    deploy app

--- app/api/cron/fetch-scores/route.ts
+++ app/api/cron/fetch-scores/route.ts
@@ -1,8 +1,18 @@
-{
-  "crons": [
-    {
-      "path": "/api/fetch-scores",
-      "schedule": "*/5 * * * *"
-    }
-  ]
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    return NextResponse.json({
+      success: true,
+      message: "cron working",
+    });
+  } catch (error: any) {
+    return NextResponse.json(
+      {
+        success: false,
+        error: error.message,
+      },
+      { status: 500 }
+    );
+  }
 }
